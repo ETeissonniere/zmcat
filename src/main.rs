24077d619483053ea23ee8b8bbbe354cfc3f6352
Eliott Teissonniere <git.eliott@teissonniere.org>
2022-07-02 12:57:01 +0200

    simple proxy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Specify the host and port to receive messages in URL format.
+    #[clap(short, long, default_value = "tcp://*:5555")]
+    frontend: String,
+
+    /// Specify the host and port to forward messages in URL format.
+    #[clap(short, long, default_value = "tcp://*:6666")]
+    backend: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let context = zmq::Context::new();
+    let frontend = context.socket(zmq::SUB).unwrap();
+    let backend = context.socket(zmq::PUB).unwrap();
+
+    frontend
+        .bind(&cli.frontend)
+        .expect("failed binding frontend");
+    backend.bind(&cli.backend).expect("failed binding backend");
+
+    zmq::proxy(&frontend, &backend).expect("failed to proxy");
 }
